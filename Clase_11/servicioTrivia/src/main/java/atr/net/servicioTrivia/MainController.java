685447584fe3895a23b4d353a1fda38312f8e2b4
Abdel Trochez <63329393+AbdelTrochez@users.noreply.github.com>
2023-06-20 22:52:25 -0600

    Mejora Frontend y Agregado Preguntas
    Se han agregado las categorias al backend y una pregunta por categoria.
    Tambien se resuelve el problema del frontend en donde en pantalla se nos muestra seleccionada una categoria pero se muestra la pregunta de otra categoria.

--- Clase_11/servicioTrivia/src/main/java/atr/net/servicioTrivia/MainController.java
+++ Clase_11/servicioTrivia/src/main/java/atr/net/servicioTrivia/MainController.java
@@ -10,36 +10,129 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @GetMapping("/question/{categoria}")
-    public Question getQuestions(@PathVariable String categoria){
+    public Question getQuestion(@PathVariable String categoria){
 
-        if(categoria.equals("Arte")){}
+        Question pregunta = getPregunta(categoria);
 
-        Question pregunta1 = new Question();
-
-        pregunta1.setCategory("Cultura");
-        pregunta1.setQuestion("¿Qué ingredintes se necesitan para hacer una michelada?");
-        String[] opciones = {"Cerveza, lima/limón, sal, chile en polvo, salsa tipo inglesa, picante", "Cerveza, lima/limón y limon", "Cerveza, sal, tabasco y lima/limón"};
-        pregunta1.setOptions(opciones);
-        pregunta1.setAnswer(0);
-        pregunta1.setExplanation("La michelada, originaria de México, nacida de la creatividad y el deseo de refrescar, se convirtió en una popular bebida que cautivó a los amantes de la cerveza con su combinación única de sabores y su espíritu festivo.");
-
-
-        return pregunta1;
+        return pregunta;
     }
 
     @GetMapping("/categories")
     public Categories[] getCategories(){
 
-        Categories categoria1 = new Categories();
-        categoria1.setCategory("Arte");
-        categoria1.setDescription("Preguntas relacionadas con pintura, escultura, arquitectura y otras formas de expresión artística.");
+        Categories categoriaCultura = new Categories();
+        categoriaCultura.setCategory("Cultura");
+        categoriaCultura.setDescription("Preguntas relacionadas con arte, literatura, música y otras expresiones culturales.");
 
-        Categories categoria2 = new Categories();
-        categoria2.setCategory("Deportes");
-        categoria2.setDescription("Preguntas relacionadas con diversos deportes y eventos deportivos.");
+        Categories categoriaDeportes = new Categories();
+        categoriaDeportes.setCategory("Deportes");
+        categoriaDeportes.setDescription("Preguntas relacionadas con diversos deportes y eventos deportivos.");
 
-         Categories[] categorias = {categoria1,categoria2};
+        Categories categoriaArte = new Categories();
+        categoriaArte.setCategory("Arte");
+        categoriaArte.setDescription("Preguntas relacionadas con pintura, escultura, arquitectura y otras formas de expresión artística.");
+
+        Categories categoriaCine = new Categories();
+        categoriaCine.setCategory("Cine");
+        categoriaCine.setDescription("Preguntas relacionadas con películas, directores, actores y otros aspectos del cine.");
+
+        Categories categoriaHistoria = new Categories();
+        categoriaHistoria.setCategory("Historia");
+        categoriaHistoria.setDescription("Preguntas relacionadas con eventos históricos, personajes y períodos importantes.");
+
+        Categories categoriaCiencia = new Categories();
+        categoriaCiencia.setCategory("Ciencia");
+        categoriaCiencia.setDescription("Preguntas relacionadas con diferentes ramas de la ciencia, descubrimientos y avances científicos.");
+
+         Categories[] categorias = {categoriaCultura,categoriaDeportes, categoriaArte, categoriaCine, categoriaHistoria, categoriaCiencia};
 
         return categorias;
+    }
+
+    private Question getPregunta(String categoria){
+        Question pregunta = new Question();
+
+        if(categoria.equals("Cultura")) pregunta = getPreguntaCultura();
+        if(categoria.equals("Deportes")) pregunta = getPreguntaDeportes();
+        if(categoria.equals("Arte")) pregunta = getPreguntaArte();
+        if(categoria.equals("Cine")) pregunta = getPreguntaCine();
+        if(categoria.equals("Historia")) pregunta = getPreguntaHistoria();
+        if(categoria.equals("Ciencia")) pregunta = getPreguntaCiencia();
+
+        return pregunta;
+    }
+    private Question getPreguntaCultura(){
+
+        Question preguntaArte = new Question();
+
+        preguntaArte.setCategory("Cultura");
+        preguntaArte.setQuestion("¿Qué ingredintes se necesitan para hacer una michelada?");
+        String[] opciones = {"Cerveza, lima/limón, sal, chile en polvo, salsa tipo inglesa, picante", "Cerveza, lima/limón y limon", "Cerveza, sal, tabasco y lima/limón"};
+        preguntaArte.setOptions(opciones);
+        preguntaArte.setAnswer(0);
+        preguntaArte.setExplanation("La michelada, originaria de México, nacida de la creatividad y el deseo de refrescar, se convirtió en una popular bebida que cautivó a los amantes de la cerveza con su combinación única de sabores y su espíritu festivo.");
+
+        return preguntaArte;
+    }
+
+    private Question getPreguntaDeportes(){
+        Question preguntaDeportes = new Question();
+
+        preguntaDeportes.setCategory("Deportes");
+        preguntaDeportes.setQuestion("¿Cuál es el deporte más popular en el mundo?");
+        String[] opcionesDeportes = {"Fútbol", "Baloncesto", "Tenis"};
+        preguntaDeportes.setOptions(opcionesDeportes);
+        preguntaDeportes.setAnswer(0);
+        preguntaDeportes.setExplanation("El fútbol es ampliamente considerado como el deporte más popular en el mundo, con miles de millones de seguidores y una amplia participación a nivel mundial.");
+
+        return preguntaDeportes;
+    }
+    private Question getPreguntaArte(){
+        Question preguntaArte = new Question();
+
+        preguntaArte.setCategory("Arte");
+        preguntaArte.setQuestion("¿Quién pintó la Mona Lisa?");
+        String[] opcionesArte = {"Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh"};
+        preguntaArte.setOptions(opcionesArte);
+        preguntaArte.setAnswer(0);
+        preguntaArte.setExplanation("La Mona Lisa es una famosa pintura renacentista creada por el artista italiano Leonardo da Vinci.");
+
+        return preguntaArte;
+    }
+    private Question getPreguntaCine(){
+        Question preguntaCine = new Question();
+
+        preguntaCine.setCategory("Cine");
+        preguntaCine.setQuestion("¿Quién dirigió la película \"El Padrino\"?");
+        String[] opcionesCine = {"Francis Ford Coppola", "Martin Scorsese", "Steven Spielberg"};
+        preguntaCine.setOptions(opcionesCine);
+        preguntaCine.setAnswer(0);
+        preguntaCine.setExplanation("La película \"El Padrino\" fue dirigida por Francis Ford Coppola, y es considerada como una de las mejores películas de la historia del cine.");
+
+        return preguntaCine;
+    }
+    private Question getPreguntaHistoria(){
+        Question preguntaHistoria = new Question();
+
+        preguntaHistoria.setCategory("Historia");
+        preguntaHistoria.setQuestion("¿En qué año se firmó la Declaración de Independencia de los Estados Unidos?");
+        String[] opcionesHistoria = {"1776", "1789", "1812"};
+        preguntaHistoria.setOptions(opcionesHistoria);
+        preguntaHistoria.setAnswer(0);
+        preguntaHistoria.setExplanation("La Declaración de Independencia de los Estados Unidos fue firmada en el año 1776, marcando el inicio de la independencia de las colonias americanas respecto al Reino de Gran Bretaña.");
+
+        return preguntaHistoria;
+    }
+    private Question getPreguntaCiencia(){
+        Question preguntaCiencia = new Question();
+
+        preguntaCiencia.setCategory("Ciencia");
+        preguntaCiencia.setQuestion("¿Quién propuso la teoría de la relatividad?");
+        String[] opcionesCiencia = {"Albert Einstein", "Isaac Newton", "Stephen Hawking"};
+        preguntaCiencia.setOptions(opcionesCiencia);
+        preguntaCiencia.setAnswer(0);
+        preguntaCiencia.setExplanation("La teoría de la relatividad fue propuesta por Albert Einstein, revolucionando nuestra comprensión del espacio, el tiempo y la gravedad.");
+
+        return preguntaCiencia;
     }
 }
